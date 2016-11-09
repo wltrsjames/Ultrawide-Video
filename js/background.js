26893c91a8a72e9f23f6e6051d556b464413a9d3
Unknown <wltrsjames@gmail.com>
2016-11-09 20:39:35 +0000

    Add: New Update popup and contributer for v1.4

--- js/background.js
+++ js/background.js
@@ -2,8 +2,7 @@ chrome.runtime.onInstalled.addListener(function (details) {
     var manifest = chrome.runtime.getManifest();
     var version = (manifest.version);
     if(details.reason == "update") {
-        alert("Update log: Ultrawide Video is back! As of :"+version+" Fixes have been applied to improve stability"+
-             ", ctrl+alt+c to toggle");
+        alert("Update log: Ultrawide Video is back! As of :"+version+" HBO GO has been added as a supported media player.");
     }
     chrome.storage.local.set({"extensionIsEnabled":false},function (){
         //console.log("Storage Succesful");
