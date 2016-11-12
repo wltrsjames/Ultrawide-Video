ce9fda6c4457acf933e7581e5aeb7d2e6e8128cb
Duncan Steenburgh <dst33nburgh@gmail.com>
2016-11-12 11:55:12 -0600

    Display notification instead of opening new tab when extension is updated

--- js/background.js
+++ js/background.js
@@ -2,8 +2,20 @@ chrome.runtime.onInstalled.addListener(function (details) {
     var manifest = chrome.runtime.getManifest();
     var version = (manifest.version);
     if(details.reason == "update") {
-        var newURL = "https://github.com/wltrsjames/Ultrawide-Video";
-        chrome.tabs.create({ url: newURL });
+        chrome.notifications.create(null, {
+            type: "basic",
+            iconUrl: "images/icon.png",
+            title: "Ultrawide Video updated",
+            message: "Update log: Ultrawide Video is back! As of version "+version+" HBO GO has been added as a supported media player.",
+        }, function(updateNotificationId) {
+            chrome.notifications.onClicked.addListener(function(notificationId) {
+                if (notificationId === updateNotificationId) {
+                    var newURL = "https://github.com/wltrsjames/Ultrawide-Video";
+                    chrome.tabs.create({ url: newURL });
+                    chrome.notifications.clear(updateNotificationId);
+                }
+            });
+        });
     }
     chrome.storage.local.set({"extensionIsEnabled":false},function (){
         //console.log("Storage Succesful");
