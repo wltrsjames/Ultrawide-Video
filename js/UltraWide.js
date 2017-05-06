54a21c0eed7889faee4c6b2139cfe2d462dc0f0f
Charlie Briggs <charlie.briggs@gmail.com>
2017-05-06 14:30:47 +0100

    Run codebase through prettier
    Code style was inconsistent, ran through https://github.com/prettier/prettier

--- js/UltraWide.js
+++ js/UltraWide.js
@@ -1,41 +1,39 @@
 UltraWide.prototype.classCheck = function() {
     //    console.log(this.scale, this.fullscreen);
-    switch(this.mode) {
-            // 0: off; 1: aspect; 2: zoom;
+    switch (this.mode) {
+        // 0: off; 1: aspect; 2: zoom;
         case 0:
-            $("video").removeClass("extraClassAspect");
-            $("video").removeClass("extraClassCrop");
+            $('video').removeClass('extraClassAspect');
+            $('video').removeClass('extraClassCrop');
             break;
         case 1:
-            if(this.fullscreen && this.scale > 1) {
-                $("video").addClass("extraClassAspect");
-            }else{
-                $("video").removeClass("extraClassCrop");
-                $("video").removeClass("extraClassAspect");
+            if (this.fullscreen && this.scale > 1) {
+                $('video').addClass('extraClassAspect');
+            } else {
+                $('video').removeClass('extraClassCrop');
+                $('video').removeClass('extraClassAspect');
             }
             break;
         case 2:
-            if(this.fullscreen && this.scale > 1) {
-                $("video").addClass("extraClassCrop");
-            }else{
-                $("video").removeClass("extraClassAspect");
-                $("video").removeClass("extraClassCrop");
+            if (this.fullscreen && this.scale > 1) {
+                $('video').addClass('extraClassCrop');
+            } else {
+                $('video').removeClass('extraClassAspect');
+                $('video').removeClass('extraClassCrop');
             }
             break;
         case 3:
-            $("video").removeClass("extraClassAspect");
-            $("video").addClass("extraClassCrop");
+            $('video').removeClass('extraClassAspect');
+            $('video').addClass('extraClassCrop');
             break;
         case 4:
-            $("video").addClass("extraClassAspect");
-            $("video").removeClass("extraClassCrop");
+            $('video').addClass('extraClassAspect');
+            $('video').removeClass('extraClassCrop');
             break;
     }
-
 };
 
-
-function UltraWide()  {
+function UltraWide() {
     this.scale = undefined;
     this.fullscreen = false;
     this.mode = 0;
@@ -46,63 +44,65 @@ function UltraWide()  {
         var height = screen.height;
 
         //get aspet ratio
-        var aspect = width/height;
+        var aspect = width / height;
 
         //16:9 = 1.77
 
-        if(aspect >= 1.88) {
+        if (aspect >= 1.88) {
             var scale = aspect / 1.77;
             this.scale = Math.round(scale * 100) / 100;
-
-        }else if(this.mode == 3 || this.mode == 4) {
+        } else if (this.mode == 3 || this.mode == 4) {
             this.scale = 1.33;
-        }else {
+        } else {
             this.scale = 1;
         }
-
     };
 
     this.fullscreenSet = function(cb) {
-        setTimeout((function() {
-            if (document.webkitCurrentFullScreenElement != null) {
-                this.fullscreen = true;
-            }else{
-                this.fullscreen = false;
-            }   
-            this.classCheck();
-        }).bind(this), 100);
+        setTimeout(
+            function() {
+                if (document.webkitCurrentFullScreenElement != null) {
+                    this.fullscreen = true;
+                } else {
+                    this.fullscreen = false;
+                }
+                this.classCheck();
+            }.bind(this),
+            100
+        );
     };
 
     this.createCSS = function() {
         $('#extraClass').remove();
 
-        var sheet = document.createElement('style')
-        sheet.setAttribute("id", "extraClass");
-        sheet.innerHTML = 
-            ".extraClassAspect {" +
-            "-webkit-transform: scaleX("+this.scale+")!important;" +
+        var sheet = document.createElement('style');
+        sheet.setAttribute('id', 'extraClass');
+        sheet.innerHTML =
+            '.extraClassAspect {' +
+            '-webkit-transform: scaleX(' +
+            this.scale +
+            ')!important;' +
             //                "object-fit: fill!important;" +
-            "}" +
-            ".extraClassCrop {" +
-            "-webkit-transform: scale("+this.scale+")!important;" +
+            '}' +
+            '.extraClassCrop {' +
+            '-webkit-transform: scale(' +
+            this.scale +
+            ')!important;' +
             //                "object-fit: cover!important;" +
-            "}";
-        document.body.appendChild(sheet);  
+            '}';
+        document.body.appendChild(sheet);
     };
 
     this.setMode = function(mode) {
         this.mode = mode;
         return mode;
     };
-
-
-};
+}
 
 var ultraWide = new UltraWide();
 
 $(document).ready(function() {
-
-    chrome.storage.local.get("extensionMode",function (status){
+    chrome.storage.local.get('extensionMode', function(status) {
         ultraWide.setMode(status.extensionMode);
         ultraWide.setScale();
         ultraWide.fullscreenSet();
@@ -110,33 +110,27 @@ $(document).ready(function() {
 
         initEvents(ultraWide);
     });
-
-
 });
 
 var initEvents = function(ultraWide) {
-
-    $( window ).resize(function() {
+    $(window).resize(function() {
         ultraWide.setScale();
         ultraWide.fullscreenSet();
         ultraWide.createCSS();
     });
 
-    $(document).on('keydown', null, 'alt+ctrl+c',function(event) {
+    $(document).on('keydown', null, 'alt+ctrl+c', function(event) {
         var state = 0;
-        if(ultraWide.mode <= 1) {
-            state = ultraWide.setMode(ultraWide.mode+1);
-        }else{
+        if (ultraWide.mode <= 1) {
+            state = ultraWide.setMode(ultraWide.mode + 1);
+        } else {
             state = ultraWide.setMode(0);
         }
 
-        chrome.storage.local.set({"extensionMode":state},function (){
-        });
-
-
+        chrome.storage.local.set({ extensionMode: state }, function() {});
     });
 
-    chrome.storage.onChanged.addListener(function(changes){
+    chrome.storage.onChanged.addListener(function(changes) {
         ultraWide.setMode(changes.extensionMode.newValue);
         ultraWide.setScale();
         ultraWide.createCSS();
--- js/popup.js
+++ js/popup.js
@@ -1,69 +1,61 @@
 $(document).ready(function() {
     var isOn = -1;
-    chrome.storage.local.get("extensionMode",function (results){
+    chrome.storage.local.get('extensionMode', function(results) {
         var mode = results.extensionMode;
-        switch(mode) {
-                // 0: off; 1: aspect; 2: zoom;
+        switch (mode) {
+            // 0: off; 1: aspect; 2: zoom;
             case 0:
-                $("#off").prop("checked", true);
+                $('#off').prop('checked', true);
                 break;
             case 1:
-                $("#aspect").prop("checked", true);
+                $('#aspect').prop('checked', true);
                 break;
             case 2:
-                $("#crop").prop("checked", true);
+                $('#crop').prop('checked', true);
                 break;
             case 3:
-                $("#forceCrop").prop("checked", true);
+                $('#forceCrop').prop('checked', true);
                 break;
             case 4:
-                $("#forceAspect").prop("checked", true);
+                $('#forceAspect').prop('checked', true);
                 break;
         }
     });
-    $("#off").click(function() {
-        $("#aspect").prop("checked", false);
-        $("#crop").prop("checked", false);
-        $("#forceCrop").prop("checked", false);
-        $("#forceAspect").prop("checked", false);
-        chrome.storage.local.set({"extensionMode":0},function (){
-        });
+    $('#off').click(function() {
+        $('#aspect').prop('checked', false);
+        $('#crop').prop('checked', false);
+        $('#forceCrop').prop('checked', false);
+        $('#forceAspect').prop('checked', false);
+        chrome.storage.local.set({ extensionMode: 0 }, function() {});
     });
 
-    $("#aspect").click(function() {
-        $("#off").prop("checked", false);
-        $("#crop").prop("checked", false);
-        $("#forceCrop").prop("checked", false);
-        $("#forceAspect").prop("checked", false);
-        chrome.storage.local.set({"extensionMode":1},function (){
-        });
+    $('#aspect').click(function() {
+        $('#off').prop('checked', false);
+        $('#crop').prop('checked', false);
+        $('#forceCrop').prop('checked', false);
+        $('#forceAspect').prop('checked', false);
+        chrome.storage.local.set({ extensionMode: 1 }, function() {});
     });
 
-    $("#crop").click(function() {
-        $("#off").prop("checked", false);
-        $("#aspect").prop("checked", false);
-        $("#forceCrop").prop("checked", false);
-        $("#forceAspect").prop("checked", false);
-        chrome.storage.local.set({"extensionMode":2},function (){
-        });
+    $('#crop').click(function() {
+        $('#off').prop('checked', false);
+        $('#aspect').prop('checked', false);
+        $('#forceCrop').prop('checked', false);
+        $('#forceAspect').prop('checked', false);
+        chrome.storage.local.set({ extensionMode: 2 }, function() {});
     });
-    $("#forceCrop").click(function() {
-        $("#off").prop("checked", false);
-        $("#aspect").prop("checked", false);
-        $("#crop").prop("checked", false);
-        $("#forceAspect").prop("checked", false);
-        chrome.storage.local.set({"extensionMode":3},function (){
-        });
+    $('#forceCrop').click(function() {
+        $('#off').prop('checked', false);
+        $('#aspect').prop('checked', false);
+        $('#crop').prop('checked', false);
+        $('#forceAspect').prop('checked', false);
+        chrome.storage.local.set({ extensionMode: 3 }, function() {});
     });
-    $("#forceAspect").click(function() {
-        $("#off").prop("checked", false);
-        $("#aspect").prop("checked", false);
-        $("#crop").prop("checked", false);
-        $("#forceCrop").prop("checked", false);
-        chrome.storage.local.set({"extensionMode":4},function (){
-        });
+    $('#forceAspect').click(function() {
+        $('#off').prop('checked', false);
+        $('#aspect').prop('checked', false);
+        $('#crop').prop('checked', false);
+        $('#forceCrop').prop('checked', false);
+        chrome.storage.local.set({ extensionMode: 4 }, function() {});
     });
 });
-
-
-
