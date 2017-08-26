7bd2f847c02f05b441ab392b38d5407ce21f8f2c
Bryce <Pecacheu@gmail.com>
2017-08-26 12:01:05 -0400

    Further Improved + iFrame Support! (#15)
    * Redesigned UI for a better UX. Refactors popup.js to be DRY.
    
    * Boo JQuerry!
    
    Seriously guys, using JQuerry for something this simple? Also made
    subtle improvments to popup window CSS, removed reliance on setTimeout
    in favor of event listeners, and obsesivly changed spaces to tabs!
    
    Plus, impoved compatibility with iFrame based players such as on, um...
    "alternative" content sources... (Shhh, the DMCA people are watching.)

--- js/popup.js
+++ js/popup.js
@@ -1,69 +1,40 @@
-$(document).ready(function() {
-    var isOn = -1;
-    chrome.storage.local.get("extensionMode",function (results){
-        var mode = results.extensionMode;
-        switch(mode) {
-                // 0: off; 1: aspect; 2: zoom;
-            case 0:
-                $("#off").prop("checked", true);
-                break;
-            case 1:
-                $("#aspect").prop("checked", true);
-                break;
-            case 2:
-                $("#crop").prop("checked", true);
-                break;
-            case 3:
-                $("#forceCrop").prop("checked", true);
-                break;
-            case 4:
-                $("#forceAspect").prop("checked", true);
-                break;
-        }
-    });
-    $("#off").click(function() {
-        $("#aspect").prop("checked", false);
-        $("#crop").prop("checked", false);
-        $("#forceCrop").prop("checked", false);
-        $("#forceAspect").prop("checked", false);
-        chrome.storage.local.set({"extensionMode":0},function (){
-        });
-    });
+var modeMap = {
+	disabled:0, aspect:1, crop:2,
+	forceCrop:3, forceAspect:4
+};
 
-    $("#aspect").click(function() {
-        $("#off").prop("checked", false);
-        $("#crop").prop("checked", false);
-        $("#forceCrop").prop("checked", false);
-        $("#forceAspect").prop("checked", false);
-        chrome.storage.local.set({"extensionMode":1},function (){
-        });
-    });
+window.onload = function() {
+	const modes = modesSel.children;
+	
+	//Set active mode
+	for(var i=0,l=modes.length; i<l; i++) modes[i].onclick = onModeClick;
+	
+	//Get current active mode
+	chrome.storage.local.get('extensionMode', function(results) {
+		var mode = Object.keys(modeMap)[results.extensionMode];
+		for(let i=0,l=modes.length,inp; i<l; i++) {
+			inp = modes[i].children[0];
+			if(inp.id == mode) { inp.checked = true; break; }
+		}
+	});
+	
+	//Show help & about
+	helpShow.onclick = function(e) {
+		e.preventDefault(); help.className = 'is-active';
+	}
+	helpHide.onclick = function(e) {
+		e.preventDefault(); help.className = null;
+	}
+	
+	//Attribution links
+	const aLinks = links.children;
+	for(let i=0,l=aLinks.length; i<l; i++) if(aLinks[i].tagName == 'A') {
+		aLinks[i].onclick = function() { chrome.tabs.create({url:this.href}); };
+	}
+}
 
-    $("#crop").click(function() {
-        $("#off").prop("checked", false);
-        $("#aspect").prop("checked", false);
-        $("#forceCrop").prop("checked", false);
-        $("#forceAspect").prop("checked", false);
-        chrome.storage.local.set({"extensionMode":2},function (){
-        });
-    });
-    $("#forceCrop").click(function() {
-        $("#off").prop("checked", false);
-        $("#aspect").prop("checked", false);
-        $("#crop").prop("checked", false);
-        $("#forceAspect").prop("checked", false);
-        chrome.storage.local.set({"extensionMode":3},function (){
-        });
-    });
-    $("#forceAspect").click(function() {
-        $("#off").prop("checked", false);
-        $("#aspect").prop("checked", false);
-        $("#crop").prop("checked", false);
-        $("#forceCrop").prop("checked", false);
-        chrome.storage.local.set({"extensionMode":4},function (){
-        });
-    });
-});
-
-
-
+function onModeClick(e) {
+	if(e.target.tagName == 'INPUT') {
+		chrome.storage.local.set({'extensionMode':modeMap[e.target.id]}, function(){});
+	}
+}
